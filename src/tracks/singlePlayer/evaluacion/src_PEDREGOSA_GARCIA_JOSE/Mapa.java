7b1589238a57a080020cafd268b5d2f3ce0f1bdc
Jose Pedregosa <josepedregos@correo.ugr.es>
2026-03-24 19:26:39 +0100

    extraccion de las casillas

--- src/tracks/singlePlayer/evaluacion/src_PEDREGOSA_GARCIA_JOSE/Mapa.java
+++ src/tracks/singlePlayer/evaluacion/src_PEDREGOSA_GARCIA_JOSE/Mapa.java
@@ -6,23 +6,31 @@ import java.util.ArrayList;
 import core.vgdl.VGDLRegistry;
 
 public class Mapa {
+    private static final byte AGUA = 1;
+    private static final byte PARED = 2;
+    private static final byte PUERTA = 3;
+    private static final byte CATNORTH = 1;
+    private static final byte CATSOUTH = 2;
+    private static final byte CATEAST = 3;
+    private static final byte CATWEST = 4;
+
     public int ancho;
     public int alto;
 
-    public short grid[][];
+    public byte grid[][];
     public boolean mapaMonedas[][];
     public boolean mapaLlaves[][];
-    public boolean mapaCatapultas[][];
+    public byte mapaCatapultas[][];
 
     public Mapa(StateObservation stateObs) {
         ArrayList<Observation>[][] obsGrid = stateObs.getObservationGrid();
         this.ancho = obsGrid.length;
         this.alto = obsGrid[0].length;
         stateObs.getAvatarType();
-        grid = new short[ancho][alto];
+        grid = new byte[ancho][alto];
         mapaMonedas = new boolean[ancho][alto];
         mapaLlaves = new boolean[ancho][alto];
-        mapaCatapultas = new boolean[ancho][alto];
+        mapaCatapultas = new byte[ancho][alto];
         
         // Obstáculos
         int idWall = VGDLRegistry.GetInstance().getRegisteredSpriteValue("wall");
@@ -41,16 +49,43 @@ public class Mapa {
         int idCatSouth = VGDLRegistry.GetInstance().getRegisteredSpriteValue("southfacing");
         int idCatEast = VGDLRegistry.GetInstance().getRegisteredSpriteValue("eastfacing");
         int idCatWest = VGDLRegistry.GetInstance().getRegisteredSpriteValue("westfacing");
+
+        for (int x = 0; x < ancho; x++) {
+            for (int y = 0; y < alto; y++) {
+                for (Observation obs: obsGrid[x][y]) {
+                    if (obs.itype == idWall)
+                        grid[x][y] = PARED;
+                    else if (obs.itype == idWater)
+                        grid[x][y] = AGUA;
+                    else if (obs.itype == idLocked)
+                        grid[x][y] = PUERTA;
+                    else if (obs.itype == idUnlocked)
+                        grid[x][y] = PUERTA;
+                    else if (obs.itype == idCoins)
+                        mapaMonedas[x][y] = true;
+                    else if (obs.itype == idKey)
+                        mapaLlaves[x][y] = true;
+                    else if (obs.itype == idCatNorth)
+                        mapaCatapultas[x][y] = CATNORTH;
+                    else if (obs.itype == idCatSouth)
+                        mapaCatapultas[x][y] = CATSOUTH;
+                    else if (obs.itype == idCatEast)
+                        mapaCatapultas[x][y] = CATEAST;
+                    else if (obs.itype == idCatWest)
+                        mapaCatapultas[x][y] = CATWEST;
+                }
+            }
+        }
     }
 
     public Mapa(int ancho, int alto) {
         this.ancho = ancho;
         this.alto = alto;
 
-        grid = new short[ancho][alto];
+        grid = new byte[ancho][alto];
         mapaMonedas = new boolean[ancho][alto];
         mapaLlaves = new boolean[ancho][alto];
-        mapaCatapultas = new boolean[ancho][alto];
+        mapaCatapultas = new byte[ancho][alto];
     }
 
     public Mapa copy(Mapa other)
