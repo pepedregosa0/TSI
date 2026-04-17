5e2695519e737b90ee7fa7dd3554b9fd8280b8ea
Jose Pedregosa <josepedregos@correo.ugr.es>
2026-04-17 19:26:12 +0200

    nose pero vale

--- src/tracks/singlePlayer/evaluacion/src_PEDREGOSA_GARCIA_JOSE/AgenteLRTAStarK.java
+++ src/tracks/singlePlayer/evaluacion/src_PEDREGOSA_GARCIA_JOSE/AgenteLRTAStarK.java
@@ -8,6 +8,7 @@ import ontology.Types.ACTIONS;
 import tools.ElapsedCpuTimer;
 import tracks.singlePlayer.MetricsProvider;
 
+import java.util.LinkedList;
 // Nota: Se puede heredar de otras clases personalizadas por el alumnado que hereden de AbstractPlayer para generalizar
 // los elementos comunes a todos los algoritmos.
 
@@ -17,15 +18,19 @@ public class AgenteLRTAStarK extends AgenteHeuristico {
 
     private int nodosExpandidos;
     private int numAcciones;
+    private int actualizacionesHeuristica;
     private long tiempoEjecucion;
 
     private HashMap<Nodo, Integer> tablaHeuristica;
+    private HashMap<Nodo, Nodo> tablaSoportes;
 
     public AgenteLRTAStarK(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
         super(stateObs, elapsedTimer);
         nodosExpandidos = 0;
         numAcciones = 0;
+        actualizacionesHeuristica = 0;
         tablaHeuristica = new HashMap<>();
+        tablaSoportes = new HashMap<>();
     }
     @Override
     public ACTIONS act(StateObservation stateObs, ElapsedCpuTimer elapsedTimer) {
@@ -38,6 +43,7 @@ public class AgenteLRTAStarK extends AgenteHeuristico {
             return ACTIONS.ACTION_NIL;
         }
         long inicio = System.nanoTime();
+        lookAheadUpdateK(nodoActual);
         ACTIONS siguienteAccion = LRTAStarK(nodoActual);
         long fin = System.nanoTime();
 
@@ -45,6 +51,7 @@ public class AgenteLRTAStarK extends AgenteHeuristico {
         MetricsProvider.getInstance().setTiempoMilisegundos(tiempoEjecucion / 1000000);
         MetricsProvider.getInstance().setNodosExpandidos(nodosExpandidos);
         MetricsProvider.getInstance().setNumAccionesPlan(numAcciones);
+        MetricsProvider.getInstance().setNumActualizacionesTabla(actualizacionesHeuristica);
         MetricsProvider.getInstance().printMetrics();
         return siguienteAccion;
     }
@@ -59,13 +66,13 @@ public class AgenteLRTAStarK extends AgenteHeuristico {
             MetricsProvider.getInstance().setNodosExpandidos(nodosExpandidos);
             MetricsProvider.getInstance().setNumAccionesPlan(numAcciones);
             MetricsProvider.getInstance().setTiempoMilisegundos(tiempoEjecucion / 1000000);
+            MetricsProvider.getInstance().setNumActualizacionesTabla(actualizacionesHeuristica);
             MetricsProvider.getInstance().printMetrics();
             return ACTIONS.ACTION_NIL;
         }
 
         Nodo mejorVecino = null;
         int minF = Integer.MAX_VALUE;
-        int segundoMinF = Integer.MAX_VALUE;
 
         // Enconctrar los dos mejores veciones según f(n) = g(n) + h(n)
         for (Nodo vecino : vecinos) {
@@ -73,23 +80,14 @@ public class AgenteLRTAStarK extends AgenteHeuristico {
             int fVecino = 1 + heuristica(vecino); 
             
             if (fVecino < minF) {
-                segundoMinF = minF;
                 minF = fVecino;
                 mejorVecino = vecino;
             }
-            else if (fVecino < segundoMinF) {
-                segundoMinF = fVecino;
-            }
-        }
-
-        // Si no hay segundo mínimo elegimos el primer minimo como valor
-        if (segundoMinF == Integer.MAX_VALUE) {
-            segundoMinF = minF;
         }
 
         // Regla de aprendizaje (Espacio local de aprendizaje)
         int hActual = heuristica(nodoActual);
-        int nuevaHeuristica = Math.max(hActual, segundoMinF);
+        int nuevaHeuristica = Math.max(hActual, minF);
         tablaHeuristica.put(nodoActual, nuevaHeuristica);
 
         ACTIONS accionSeleccionada = mejorVecino.getAccionPadre();
@@ -99,6 +97,53 @@ public class AgenteLRTAStarK extends AgenteHeuristico {
         MetricsProvider.getInstance().setNumAccionesPlan(numAcciones);
 
         return accionSeleccionada;
+    }
+
+    private void lookAheadUpdateK(Nodo nodoInicial) {
+        LinkedList<Nodo> cola = new LinkedList<>();
+        cola.add(nodoInicial);
+
+        int contador = k - 1;
+        while (!cola.isEmpty()) {
+            Nodo x = cola.poll();
+        
+            ArrayList<Nodo> sucesoresX = x.expandir(mapa);
+            Nodo mejorVecinoX = null;
+            int minF = Integer.MAX_VALUE;
+
+            for (Nodo sucesorX : sucesoresX) {
+                int hVecino = tablaHeuristica.getOrDefault(sucesorX, mapa.H(sucesorX.x, sucesorX.y));
+                int fVecino = 1 + hVecino;
+
+                if (fVecino < minF) {
+                    minF = fVecino;
+                    mejorVecinoX = sucesorX;
+                }
+            }
+
+            tablaSoportes.put(x, mejorVecinoX);
+
+            boolean propagar = false;
+            int hXActual = tablaHeuristica.getOrDefault(x, mapa.H(x.x, x.y));
+
+            if (hXActual < minF) {
+                tablaHeuristica.put(x, minF);
+                actualizacionesHeuristica++;
+                propagar = true;
+            }
+            
+            if (propagar) {
+                // intentamos propagar el cambio a los nodos de los que x es soporte
+                for (Nodo sucesor : sucesoresX) {
+                    Nodo soporteSucesor = tablaSoportes.get(sucesor);
+
+                    if (contador > 0 && soporteSucesor != null && soporteSucesor.equals(x)) {
+                        cola.add(sucesor);
+                        contador--;
+                    }
+                }
+            }
+        }
     }
 
     private int heuristica(Nodo nodo) {
